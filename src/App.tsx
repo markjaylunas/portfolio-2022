4f0622eb5df80186cd34de432fe264819b92cd68
mcje <markjay.lunas@gmail.com>
2022-07-24 14:00:04 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+
+
+
+function App() {
+  return (
+    <div className="container overflow-hidden bg-slate-600 text-slate-300 text-center h-[100vh] mx-auto">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+import logo from '../assets/mcje-transparent.svg'
+
+export default () => {
+    return (
+        <header className=' '>
+            <img className='bg-cover bg-left w-20'  src={logo} alt="logo" />
+            <div>Header</div>
+        </header>
+    )
+}
